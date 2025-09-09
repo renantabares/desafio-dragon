49e82208ae66faf404d91658f3de19d243110093
renantabares <renantabares@hotmail.com>
2025-09-09 08:16:25 -0300

    add login

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: "http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
